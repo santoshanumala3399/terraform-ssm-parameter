bf41e7ac518fd2463ab67fa87b5df4e5407ab21c
Santosh Anumala <santoshanumala01@gmail.com>
2025-07-13 14:51:09 +0530

    Initial commit with Terraform code

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"  # Change if needed
+}
+
+resource "aws_ssm_parameter" "example" {
+  name  = "/my/parameter"
+  type  = "String"
+  value = "HelloFromTerraform"
+}
