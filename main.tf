85cd9eb3fd9a289f25cdeb4b12fbe02dda3ac375
Santosh Anumala <santoshanumala01@gmail.com>
2025-07-13 21:46:12 +0530

    Save local changes before pulling

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-santosh2244"   # your S3 bucket name
+    key    = "ssm/terraform.tfstate"         # path for the state file inside the bucket
+    region = "us-east-1"                      # your bucket region
+  }
+}
+
+
 provider "aws" {
   region = "us-east-1"  # Change if needed
 }
@@ -5,5 +14,5 @@ provider "aws" {
 resource "aws_ssm_parameter" "example" {
   name  = "/my/parameter"
   type  = "String"
-  value = "HelloFromTerraform"
+  value = "UpdatedByTerraform"
 }
